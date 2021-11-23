9a603f1eb0fa7d7f215b808dd43a3e20eccd187b
Mubarak Pathan <pathan.mubarak@hotmail.com>
2021-11-23 19:37:26 +0530

    angle between hours and minutes

--- Microsoft/src/main/AngleBetweenHourAndMinuteHand.java
+++ Microsoft/src/main/AngleBetweenHourAndMinuteHand.java
@@ -1,12 +1,38 @@
 package main;
 
+import java.text.ParseException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class AngleBetweenHourAndMinuteHand {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ParseException {
+
+		LocalTime localTime = LocalTime.parse("11:55", DateTimeFormatter.ofPattern("HH:mm"));
+		int hour = localTime.getHour();
+		int minute = localTime.getMinute();
+		System.out.println(angleClock(hour, minute));
 		
-		System.out.println("2");
+		System.err.println();
 
 	}
 
+	public static double angleClock(int hour, int minutes) {
+
+		double angleHourHand = (hour * 30) % 360 + minutes * 0.5;
+
+		double angleMinuteHand = minutes * 6;
+
+		double difference = angleHourHand - angleMinuteHand;
+
+		if (difference < 0) {
+			difference *= -1;
+		}
+
+		if (difference > 180) {
+			difference = 360 - difference; // the other angles that completes to 360
+		}
+
+		return difference;
+	}
 }
